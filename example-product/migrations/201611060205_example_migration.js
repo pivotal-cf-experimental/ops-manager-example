1bfe83b0d39bc0dd822b3314fcdf132c8eed399a
Andrew Cantino <acantino@pivotal.io>
2016-01-18 16:34:29 -0800

    Fix console.log in migration to display correctly in Rails debug log
    Signed-off-by: Alvaro Perez-Shirley <aperezshirley@pivotal.io>

--- example-product/migrations/201611060205_example_migration.js
+++ example-product/migrations/201611060205_example_migration.js
@@ -1,4 +1,7 @@
 exports.migrate = function(properties) {
-    console.log('the properties passed to migrate are: ', properties);
-    return properties;
+  // To view JavaScript console logs in the Rails debug log, be sure to make them single value and to only pass them
+  // strings.  JSON.stringify is an easy way to dump a structure.
+  console.log('the properties passed to migrate are: ');
+  console.log(JSON.stringify(properties));
+  return properties;
 };
