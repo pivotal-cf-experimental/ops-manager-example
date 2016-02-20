777d28d5bd1d32572e261cff3479ef4379282eb2
Maryam Labib <mlabib@pivotal.io>
2016-02-19 17:01:23 -0800

    Align docs tutorial with repo.
    Signed-off-by: Caroline Taymor <ctaymor@pivotal.io>

--- example-product/migrations/201611060205_example_migration.js
+++ example-product/migrations/201611060205_example_migration.js
@@ -1,18 +1,17 @@
 exports.migrate = function(properties) {
-    //console.log('the properties passed to migrate are: ', properties);
+  // Append text to a string
+  properties['.web_server.example_string']['value'] += '!';
 
-    // Append '!'
-    properties['product_properties']['example_string']["value"] = properties['product_properties']['example_string']["value"] + '!';
+  // Rename property 'example_port' to 'example_port_renamed',
+  // retaining the previous value.
 
-    // Rename property "example_port" => "example_port_renamed"
-    properties['product_properties']['example_port_renamed'] = properties['product_properties']['example_port'];
-    delete properties['product_properties']['example_port'];
+  properties['.properties.example_port_renamed'] = properties['.properties.example_port'];
+  delete properties['.properties.example_port'];
 
-    // Append "new-string-appended-by-v1" to string_list
-    var string_list = properties['product_properties']['example_string_list']['value'].split(',');
-    string_list.push('new-string-appended-by-v1');
-    properties['product_properties']['example_string_list']['value'] = string_list.join(',');
+  // Append text to a string list
+  var string_list = properties['.properties.example_string_list']['value'].split(',');
+  string_list.push('new-string-appended-by-v1');
+  properties['.properties.example_string_list']['value'] = string_list.join(',');
 
-    console.log(properties);
-    return properties;
+  return properties;
 };
