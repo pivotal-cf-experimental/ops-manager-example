6120afe610c87d4225ac938bcaca81cafcd49fc9
Ryan Moran <rmoran@pivotal.io>
2018-11-08 14:50:21 -0800

    Add runtime config only tile
    - Runtime config only tile uses the bosh dummy release. All it does is
    write ((.properties.echo_value)) to /var/vcap/sys/log/dummy/stdout.log
    - It also has a cute crash test dummy icon
    
    [#161489491] Runtime-only configs should not show up as pending after "apply changes" has successfully completed
    
    Signed-off-by: Nick Webb <nwebb@pivotal.io>

---
+++ example-runtime-config-product/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -exu
+
+readonly PROGDIR="$(cd "$(dirname "${0}")" && pwd)"
+
+function main() {
+  local zip_file
+  zip_file="/tmp/example-runtime-config-product-2.5.0.pivotal"
+
+  pushd "${PROGDIR}" > /dev/null
+    zip -qr "${zip_file}" migrations metadata releases
+
+    echo "BUILT: ${zip_file}"
+  popd > /dev/null
+}
+
+main "${@:-}"
