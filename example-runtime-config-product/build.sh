c6a301cfbbe9e38a74425ee55e5436dcd18612bd
Nick Webb <nwebb@pivotal.io>
2019-01-10 10:11:05 -0800

    Add a new build tile script
    Add help text and additional flags for output format
    
    Add support for all types of products
    
    Default stemcell os and version to that which appears in installation
    
    Refactor build.sh to work with erb metadata
    
    Signed-off-by: Brian Upton <bupton@pivotal.io>

--- example-runtime-config-product/build.sh
+++ example-runtime-config-product/build.sh
@@ -1,16 +1,36 @@
 #!/bin/bash -exu
 
+TMP_DIR="$(mktemp -d)"
+
 readonly PROGDIR="$(cd "$(dirname "${0}")" && pwd)"
 
 function main() {
+  mkdir -p ${TMP_DIR}/metadata
+  cp -r ${PROGDIR}/migrations ${TMP_DIR}
+  cp -r ${PROGDIR}/releases ${TMP_DIR}
+
   local zip_file
   zip_file="/tmp/example-runtime-config-product-1.0.0.pivotal"
 
-  pushd "${PROGDIR}" > /dev/null
+  pushd ".." > /dev/null
+    ./generate-metadata \
+      "1.0.0" \
+      "170.16" \
+      "" \
+      "example-runtime-config-product" \
+      "" \
+      "ubuntu-xenial" \
+        > "${TMP_DIR}/metadata/example-runtime-config-product.yml"
+  popd > /dev/null
+
+  pushd "${TMP_DIR}" > /dev/null
     zip -qr "${zip_file}" migrations metadata releases
 
     echo "BUILT: ${zip_file}"
   popd > /dev/null
 }
 
+trap "rm -rf ${TMP_DIR}" EXIT
+
 main "${@:-}"
+
