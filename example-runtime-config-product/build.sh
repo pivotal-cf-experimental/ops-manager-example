2c21c48c2e9ef77b4457d8ae7df1316e698df0ce
Nick Webb <nwebb@pivotal.io>
2019-01-30 09:30:40 -0800

    Stop referencing relative paths in script

--- example-runtime-config-product/build.sh
+++ example-runtime-config-product/build.sh
@@ -3,25 +3,24 @@
 TMP_DIR="$(mktemp -d)"
 
 readonly PROGDIR="$(cd "$(dirname "${0}")" && pwd)"
+readonly EXAMPLEDIR="$(dirname ${PROGDIR})"
 
 function main() {
+  local zip_file
+  zip_file="/tmp/example-runtime-config-product-1.0.0.pivotal"
+
   mkdir -p ${TMP_DIR}/metadata
   cp -r ${PROGDIR}/migrations ${TMP_DIR}
   cp -r ${PROGDIR}/releases ${TMP_DIR}
 
-  local zip_file
-  zip_file="/tmp/example-runtime-config-product-1.0.0.pivotal"
-
-  pushd ".." > /dev/null
-    ./generate-metadata \
-      "1.0.0" \
-      "170.16" \
-      "" \
-      "example-runtime-config-product" \
-      "" \
-      "ubuntu-xenial" \
-        > "${TMP_DIR}/metadata/example-runtime-config-product.yml"
-  popd > /dev/null
+  ${EXAMPLEDIR}/generate-metadata \
+    "1.0.0" \
+    "170.16" \
+    "" \
+    "example-runtime-config-product" \
+    "" \
+    "ubuntu-xenial" \
+      > "${TMP_DIR}/metadata/example-runtime-config-product.yml"
 
   pushd "${TMP_DIR}" > /dev/null
     zip -qr "${zip_file}" migrations metadata releases
