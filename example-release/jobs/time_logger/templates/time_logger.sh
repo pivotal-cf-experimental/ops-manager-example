c0bd5171fd6c527c9069cc7e6a4d531256ca69df
Shaan Sapra <shsapra@pivotal.io>
2019-10-03 14:33:53 -0700

    Extend time_logger to print telemetry payload
    [#167629702] Extend the example product to use telemetry
    Signed-off-by: Ming Xiao <mxiao@pivotal.io>

--- example-release/jobs/time_logger/templates/time_logger.sh
+++ example-release/jobs/time_logger/templates/time_logger.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-log_path=$1
+persistent_log_path=$1
+telemetry_log_path=$2
 
-echo "PID: $$ --- starting up" >> ${log_path}
+echo "PID: $$ --- starting up" >> ${persistent_log_path}
 
 while true
 do
-  echo "PID: $$ --- `date`" >> ${log_path}
+  echo "PID: $$ --- `date`" >> ${persistent_log_path}
+  echo "{\"telemetry_source\": \"time_logger\", \"telemetry_time\": \"`date --rfc-3339=seconds`\", \"message\": \"some message\"}" >> ${telemetry_log_path}
   sleep 60
 done
