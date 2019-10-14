513a6376c41868226a7c081897cbb14474af521f
Brian Upton <bupton@pivotal.io>
2019-10-14 13:53:40 -0700

    Fix telemetry message format for time_logger
    [#167629713] Write integration tests to configure centralizer with test API key and confirm that the system works
    
    Signed-off-by: Shaan Sapra <shsapra@pivotal.io>

--- example-release/jobs/time_logger/templates/time_logger.sh
+++ example-release/jobs/time_logger/templates/time_logger.sh
@@ -10,6 +10,6 @@ echo "PID: $$ --- starting up" >> ${persistent_log_path}
 while true
 do
   echo "PID: $$ --- `date`" >> ${persistent_log_path}
-  echo "{\"telemetry_source\": \"time_logger\", \"telemetry_time\": \"`date --rfc-3339=seconds`\", \"message\": \"some message\"}" >> ${telemetry_log_path}
+  echo "{\"telemetry-source\": \"time_logger\", \"telemetry-time\": \"`date --rfc-3339=seconds`\", \"message\": \"some message\"}" >> ${telemetry_log_path}
   sleep 60
 done
